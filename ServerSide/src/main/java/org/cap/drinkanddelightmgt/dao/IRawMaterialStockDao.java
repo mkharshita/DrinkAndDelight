ed1d9d003a7edf7c0c0934aa308e8fca5e3b19a5
mkharshita <35831970+mkharshita@users.noreply.github.com>
2020-05-22 12:51:02 +0530

    Update IRawMaterialStockDao.java

--- ServerSide/src/main/java/org/cap/drinkanddelightmgt/dao/IRawMaterialStockDao.java
+++ ServerSide/src/main/java/org/cap/drinkanddelightmgt/dao/IRawMaterialStockDao.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 import org.cap.drinkanddelightmgt.entities.RawMaterialStockEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
+@Repository
 public interface IRawMaterialStockDao extends JpaRepository<RawMaterialStockEntity,String> {
 	 Optional<RawMaterialStockEntity> findByOrderId(String OrderId);
 }
